3802497fedd7ef64c5d1fb628b4e7938897f0a3a
Ruchit Marathe <ruchitmarathe@gmail.com>
2022-09-10 15:38:09 +0200

    sm7125-common: bluetooth: Set device name dynamically
    Change-Id: Ie8d19c4ab869b8a5dbb248d2cf3d24fbb7c616fe

--- bluetooth/include/bdroid_buildcfg.h
+++ bluetooth/include/bdroid_buildcfg.h
@@ -23,7 +23,30 @@
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
 
-#define BTM_DEF_LOCAL_NAME "Galaxy A52/A72 4G"
+#pragma push_macro("PROPERTY_VALUE_MAX")
+
+#if !defined(OS_GENERIC)
+#include <cutils/properties.h>
+#include <string.h>
+
+static inline const char* getBTDefaultName()
+{
+    char device[PROPERTY_VALUE_MAX];
+    property_get("ro.bootloader", device, "");
+
+    if (strstr(device, "A525") != NULL) {
+        return "Galaxy A52 4G";
+    }
+
+    if (strstr(device, "A725") != NULL) {
+        return "Galaxy A72";
+    }
+
+    return "Samsung Galaxy A52/A72 4G";
+}
+
+#define BTM_DEF_LOCAL_NAME getBTDefaultName()
+#endif /* OS_GENERIC */
 
 #define BLE_VND_INCLUDED   TRUE
 #define MAX_ACL_CONNECTIONS   16
@@ -31,5 +54,7 @@
 #define GATT_MAX_PHY_CHANNEL  10
 
 #define AVDT_NUM_SEPS 35
+
+#pragma pop_macro("PROPERTY_VALUE_MAX")
 
 #endif
