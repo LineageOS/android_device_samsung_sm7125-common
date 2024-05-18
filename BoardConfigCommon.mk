a189ef844fe2619a32ad92981e90810a9e72f134
Han Sol Jin <hansol@hansol.ca>
2024-05-18 09:42:32 -0700

    sm7125-common: explicitly include common Lineage libperfmgr sepolicy
    Change-Id: I37cfc4de02af9f7711bf1fc32aabbbe539017c24

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -194,6 +194,7 @@ TARGET_RECOVERY_UPDATER_LIBS := librecovery_updater_samsung_sm7125
 TARGET_RELEASETOOLS_EXTENSIONS := $(COMMON_PATH)/releasetools
 
 # SePolicy
+include device/lineage/sepolicy/libperfmgr/sepolicy.mk
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
 PRODUCT_PRIVATE_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/private
