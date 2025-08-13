62eeebf23ea3a95bdff5cea8c655a609c8c5f25d
LuK1337 <priv.luk@gmail.com>
2025-08-13 21:56:50 +0200

    sm7125-common: Migrate to lineagehealth IFastCharge
    Change-Id: Ia2f1f213de2f2e5afd913e5de38712fddc386b49

--- common.mk
+++ common.mk
@@ -176,10 +176,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     fastbootd
 
-# FastCharge
-PRODUCT_PACKAGES += \
-    vendor.lineage.fastcharge@1.0-service.samsung
-
 # Lineage Health
 PRODUCT_PACKAGES += \
     vendor.lineage.health-service.default
@@ -188,6 +184,9 @@ $(call soong_config_set,lineage_health,charging_control_charging_path,/sys/class
 $(call soong_config_set,lineage_health,charging_control_charging_enabled,0)
 $(call soong_config_set,lineage_health,charging_control_charging_disabled,1)
 $(call soong_config_set,lineage_health,charging_control_supports_bypass,false)
+$(call soong_config_set,lineage_health,fast_charge_node,/sys/class/sec/switch/afc_disable)
+$(call soong_config_set,lineage_health,fast_charge_value_none,1)
+$(call soong_config_set,lineage_health,fast_charge_value_fast_charge,0)
 
 # FlipFlap
 PRODUCT_PACKAGES += \
