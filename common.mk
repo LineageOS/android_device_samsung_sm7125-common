stuffbykoko/device_samsung_sm7125-common

sm7125-common: touch: Use samsung_header_path_defaults
Reference: Ibdfd255efafd471e59dfec0860ace40c21a6e27f
Change-Id: If6d25ec56dc46a4527480f13d6a084e4cca4fe44
Signed-off-by: DY3413 <milosix3413@gmail.com>

common.mk
@@ -158,6 +158,9 @@ PRODUCT_PACKAGES += \
     vendor.display.config@2.0.vendor \
     AdvancedDisplay
 
+# Include
+$(call soong_config_set,samsungVars,target_specific_header_path,$(COMMON_PATH)/include)
+
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm-service.clearkey \

LineageOS/android_device_samsung_sm7125-common

common.mk
@@ -219,6 +219,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/permissions/privapp-permissions-hotword.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-hotword.xml
 
+# Include
+$(call soong_config_set,samsungVars,target_specific_header_path,$(LOCAL_PATH)/include)
+
 # Keylayout
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/configs/keylayout/sec_touchscreen.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/sec_touchscreen.kl
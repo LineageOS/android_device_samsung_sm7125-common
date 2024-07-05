21cc17a60ebd2138a6517bb1f9460661999016e3
DY3413 <milosix3413@gmail.com>
2024-07-05 20:15:12 +0900

    sm7125-common: touch: Use samsung_header_path_defaults
    Reference: Ibdfd255efafd471e59dfec0860ace40c21a6e27f
    Change-Id: If6d25ec56dc46a4527480f13d6a084e4cca4fe44
    Signed-off-by: DY3413 <milosix3413@gmail.com>

--- common.mk
+++ common.mk
@@ -219,6 +219,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/permissions/privapp-permissions-hotword.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-hotword.xml
 
+# Include
+$(call soong_config_set,samsungVars,target_specific_header_path,$(LOCAL_PATH)/include)
+
 # Keylayout
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/configs/keylayout/sec_touchscreen.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/sec_touchscreen.kl
