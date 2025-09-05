b5f74a07a910d42e58fe0767872f18fce9b4d9b3
Bruno Martins <bgcngm@gmail.com>
2025-09-05 20:20:28 +0100

    sm7125-common: Move UDFPS config to new soong namespace
    Change-Id: Id5eb8f0f0b4962655c593a6ae5c87c4226174e3c

--- common.mk
+++ common.mk
@@ -173,7 +173,7 @@ PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint-service.samsung
 
 # UDFPS
-$(call soong_config_set,samsung_udfps,udfps_zorder,0x20000000u)
+$(call soong_config_set,samsungUdfpsVars,udfps_zorder,0x20000000u)
 $(call soong_config_set,surfaceflinger,udfps_lib,//hardware/samsung/fingerprint:libudfps_extension.samsung)
 
 # fastbootd
